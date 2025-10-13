c7d58f21e0eedb79c4e7d9deaa274f7fc5d5808b
TheDark-Cloud <117770466+TheDark-Cloud@users.noreply.github.com>
2025-10-13 07:17:08 +0000

    The the following routes works
    -utilisateur.py
    -role.py
    
    what is missing is:
    -complete_compte.py
    -login.py
    -admin.py

--- api utilisateur/extension.py
+++ api utilisateur/extension.py
@@ -1,39 +1,3 @@
-# """Shared extensions used by the Flask application."""
-# from flask_sqlalchemy import SQLAlchemy
-# db: SQLAlchemy = SQLAlchemy()
-#
-#
-# import jwt
-# from datetime import datetime, timedelta
-# from flask import current_app
-#
-# def generate_token(identity: dict):
-#     info = {
-#         "sub": str(identity.get("id_utilisateur")),              # Subject of the token
-#         "iat": datetime.utcnow(),                # Issued at
-#         "exp": datetime.utcnow() + timedelta(days=365*80)  # Expiration
-#     }
-#
-#     payload = {**info, **identity}
-#
-#     token = jwt.encode(payload, current_app.config['SECRET_KEY'], algorithm='HS256')
-#     return token
-#
-# def decode_token(token):
-#     try:
-#         payload = jwt.decode(
-#             token,
-#             current_app.config["SECRET_KEY"],
-#             algorithms=["HS256"]
-#         )
-#         return payload  # This will be a dict with your claims
-#     except jwt.ExpiredSignatureError:
-#         return {"error": "Token has expired"}
-#     except jwt.InvalidTokenError:
-#         return {"error": "Invalid token"}
-
-
-"""Shared extensions used by the Flask application."""
 from flask_sqlalchemy import SQLAlchemy
 db: SQLAlchemy = SQLAlchemy()
 
